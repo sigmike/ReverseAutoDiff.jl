e1081500ef3106245ec2b031cb328e13a55be894
Michael Witrant <michael@ouvrages-web.fr>
2013-10-19 19:45:39 +0200

    forward

--- ReverseAutoDiff.jl
+++ ReverseAutoDiff.jl
@@ -1,7 +1,27 @@
 module ReverseAutoDiff
 
-function test()
+type RADValue
+    v
+    d
+end
 
+RADValue(x) = RADValue(x, zero(x))
+
+function *(a::RADValue, b::RADValue)
+    RADValue(a.v * b.v)
+end
+
+function backpropagate(v::RADValue)
+end
+
+function test()
+    x = RADValue(3.0)
+    f() = x*x
+
+    y = f()
+    backpropagate(y)
+    @assert isequal(y.v, 9.0)
+    @assert isequal(x.d, 6.0)
 end
 
 end
